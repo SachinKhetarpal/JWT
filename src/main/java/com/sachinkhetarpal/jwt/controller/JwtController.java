838dab48272a19798387101a5a3f3e66792e87b4
SachinKhetarpal <sachin0993@gmail.com>
2022-08-05 15:37:15 +0530

    Added @CrossOrigin

--- src/main/java/com/sachinkhetarpal/jwt/controller/JwtController.java
+++ src/main/java/com/sachinkhetarpal/jwt/controller/JwtController.java
@@ -7,15 +7,13 @@ import com.sachinkhetarpal.jwt.model.JwtResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin
 public class JwtController {
 
     @Autowired
@@ -30,12 +28,13 @@ public class JwtController {
     @RequestMapping(value = "/token",method = RequestMethod.POST)
     public ResponseEntity<?> generateToken(@RequestBody JwtRequest jwtRequest) throws Exception {
 
+        System.out.println(jwtRequest.getUsername()+jwtRequest.getPassword());
         try {
             this.authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(jwtRequest.getUsername(), jwtRequest.getPassword()));
         }
         catch(Exception e){
-            e.printStackTrace();
-            throw new Exception("Bad Credentials");
+            throw new BadCredentialsException("Bad credentials");
+
         }
 
         UserDetails userDetails = this.customUserDetailsService.loadUserByUsername(jwtRequest.getUsername());
